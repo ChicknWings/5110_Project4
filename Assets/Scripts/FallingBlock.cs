891ceb4e6d350020ec7938601353d4b9025dbb34
saierjiaoyi1 <76769862+saierjiaoyi1@users.noreply.github.com>
2023-12-02 03:24:26 -0500

    destroy block based on timer

--- Assets/Scripts/FallingBlock.cs
+++ Assets/Scripts/FallingBlock.cs
@@ -1,22 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class FallingBlock : MonoBehaviour
 {
-    public float fallingSpeed = 5.0f; // fall speed
+    public float iniFallingSpeed = 5.0f; // fall speed
+    private float fallingSpeed = 5.0f;
     public float weight = 1.0f; // weight
     public bool isCaught = false; // if be caught by others
     public bool isCatchOthers = false;// if catch others
 
-    private GameObject player; 
+    public float score;
+    public float bonus = 0.2f;//如果完成了+多少分，范围0-1
+    public float punish = -0.1f;//如果失败了+多少分，范围-1-0
+    public float finishTime = 5.0f;//完成需要多少时间
+    private float timer = 0.0f;
+
+    public Image timerCircle;
+
+    public FallingBlock preivousBlock;
+    public FallingBlock nextBlock;
+
+    private GameObject player;
 
     void Start()
     {
         player = GameObject.Find("Player");
+        fallingSpeed = iniFallingSpeed;
     }
 
-    
+
     void Update()
     {
         // if not be caught, falling
@@ -24,8 +38,20 @@ public class FallingBlock : MonoBehaviour
         {
             transform.Translate(Vector2.down * fallingSpeed * Time.deltaTime);
         }
+
+        //if be caught, -time
+        if (isCaught)
+        {
+            timer += Time.deltaTime;
+            UpdateTimerCircle(timer / finishTime);
+        }
+        if (timer >= finishTime)
+        {
+            Finish();
+            timer = 0.0f;
+        }
     }
-    
+
     public void CollisionEnter(Collider2D collision)
     {
         var other = collision.gameObject.GetComponent<FallingBlock>();
@@ -36,6 +62,63 @@ public class FallingBlock : MonoBehaviour
             transform.parent = player.transform;
 
             other.isCatchOthers = true;
+
+            preivousBlock = other;
+            other.nextBlock = this;
         }
+    }
+    void UpdateTimerCircle(float progress)
+    {
+        if (timerCircle != null)
+        {
+            timerCircle.fillAmount = progress;
+        }
+    }
+
+    void Finish()
+    {
+        Debug.Log("Finished!");
+        //调用后一个物体的refall，如果有后一个物体
+        if(nextBlock != null)
+        {
+            nextBlock.ReFall();
+        }
+        //调用前一个物体的recatch
+        if(preivousBlock != null)
+        {
+            preivousBlock.Recatch();
+        }
+        preivousBlock = null;
+        nextBlock = null;
+        transform.parent = null;
+        Debug.Log("destroy:" + this.gameObject);
+        Destroy(this.gameObject);
+        //加分
+        //加进度条
+        //让自己消失
+    }
+
+    public void ReFall()//如果下面的一个block消失了，就要调用上一个block的这个方法
+    {
+        transform.parent = null;
+        isCaught = false;
+        isCatchOthers = false;
+        if (preivousBlock != null)
+        {
+            fallingSpeed = preivousBlock.fallingSpeed;
+        }
+        else
+        {
+            fallingSpeed = iniFallingSpeed;
+        }
+        if (nextBlock != null)
+        {
+            nextBlock.ReFall();
+        }
+    }
+
+    public void Recatch()
+    {
+        isCatchOthers = false;
     }
 }
