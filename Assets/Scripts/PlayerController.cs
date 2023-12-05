6a65f08c2ae7f1823be0f67a49ed51810565862b
Shaoran <yang2sr@mail.uc.edu>
2023-12-04 22:54:10 -0500

    Main menu font change + character animation

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,22 +1,33 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
 public class PlayerController : MonoBehaviour
 {
     public float moveSpeed = 5f; 
 
     private Rigidbody2D rb;
     private float moveInput;
+    private Animator animator;
+    public GameObject spriteObject;
+    
 
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        animator = GetComponentInChildren<Animator>();
     }
 
     void Update()
     {
-        moveInput = Input.GetAxis("Horizontal"); 
+        moveInput = Input.GetAxis("Horizontal");
+
+        if (moveInput > 0) 
+        {
+            animator.Play("Run");
+            
+        }
+
+           
     }
 
     void FixedUpdate()
